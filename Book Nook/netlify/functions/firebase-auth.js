fe6ed4ca7b3b41dbdacb3305581a948080bd9f5a
prav yadav <prablemo@outlook.com>
2025-02-11 16:53:58 +0545

    asdf

--- Book Nook/netlify/functions/firebase-auth.js
+++ Book Nook/netlify/functions/firebase-auth.js
@@ -4,26 +4,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebas
 import { getAuth, GoogleAuthProvider, signInWithPopup, setPersistence, browserSessionPersistence } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 // Firebase Config
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Set session persistence
-setPersistence(auth, browserSessionPersistence)
-    .then(() => console.log("Persistence set to session-only."))
-    .catch(error => console.error("Error setting persistence:", error.message));
 
 // Google Sign-In Function
-export function googleSignIn() {
+export async  function googleSignIn() {
+    const firebaseConfig = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    
+    // Set session persistence
+    setPersistence(auth, browserSessionPersistence)
+        .then(() => console.log("Persistence set to session-only."))
+        .catch(error => console.error("Error setting persistence:", error.message));
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: 'select_account' });
 
